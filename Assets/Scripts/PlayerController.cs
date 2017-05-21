9f8c94f2d24b284a13b262e62666d9cd46b645c7
Mitch Seymour <mseymour@RSG5323hSeymour.rsg.lan>
2017-05-21 14:25:35 -0400

    Add ship movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// apply movement
+		Rigidbody playerRb = GetComponent<Rigidbody> ();
+		playerRb.velocity = new Vector3 (moveHorizontal, 0.0f, moveVertical) * speed;
+
+		// make sure the player's position is within the play area
+		playerRb.position = new Vector3(
+			Mathf.Clamp(playerRb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(playerRb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		playerRb.rotation = Quaternion.Euler (0.0f, 0.0f, playerRb.velocity.x * -tilt);
+	}
+}
