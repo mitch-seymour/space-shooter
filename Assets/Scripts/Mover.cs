c230ff5561f4b9688eb547a386f4735c2d9e1338
Mitch Seymour <mseymour@RSG5323hSeymour.rsg.lan>
2017-05-26 17:47:04 -0400

    Add bolt prefab

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	public float speed;
+
+	// this will be executed on the first frame the object is instantiated
+	void Start () {
+		Rigidbody boltRb = GetComponent<Rigidbody> ();
+		boltRb.velocity = transform.forward * speed;
+	}
+
+}
