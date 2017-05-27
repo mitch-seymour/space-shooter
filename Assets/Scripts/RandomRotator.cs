3b4cf00f15b046d5785cd89caa56adce45d855ef
Mitch Seymour <mseymour@RSG5323hSeymour.rsg.lan>
2017-05-26 22:34:39 -0400

    Destroy asteroid during collision with bolt

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+	public float tumble;
+
+	void Start() {
+		Rigidbody asteroidRb = GetComponent<Rigidbody> ();
+		asteroidRb.angularVelocity = Random.insideUnitCircle * tumble;
+	}
+}
